d91cb5a0fbe8973a56a5f8f9ff82a9ef10c07c70
Keshara Madushanka <122123096+KesharaMadushanka@users.noreply.github.com>
2023-05-26 13:52:59 +0530

    Initial commit

---
+++ src/main/java/com/example/studentmanagementrestapi/StudentManagementRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmanagementrestapi/StudentManagementRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
